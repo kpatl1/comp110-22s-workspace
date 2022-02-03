3ff82081879d77bbefecff21daa4bbb104ae18e6
Kishan Patel <kpatel6@unc.edu>
2022-02-02 22:41:04 -0500

    lesson

---
+++ lessons/function_def.py
@@ -0,0 +1,9 @@
+"""" An example function definition"""
+
+
+def my_Max(a: int, b: int) -> int:
+    """Returns the greatest argument"""
+    if a >= b:
+        return a
+    else: 
+        return b
